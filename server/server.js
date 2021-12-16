206173966d8018c11cf51d497be6b5d741a445b9
Dallin Harkness <dalhark99@gmail.com>
2021-12-16 13:07:46 -0700

    rollbar

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const path = require('path')
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '516f766bc18246a2b9f2d5deb66fa01e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+
 const app = express()
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+  res.sendFile(path.join(__dirname, '../index.html'))
+  rollbar.info('html up and running')
+})
 
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'))
